f977ffccf032819dd0fc6425f49612f189b1acde
mayar-kabaja <kabajamayar@gmail.com>
2022-03-12 15:38:26 +0200

    create server Relates #1

---
+++ server/app.js
@@ -0,0 +1,16 @@
+require('env2')('.env');
+const { join } = require('path');
+const express = require('express');
+const compression = require('compression');
+const cookiParsar = require('cookie-parser');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(cookiParsar());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, '..', 'public')));
+
+module.exports = app;
