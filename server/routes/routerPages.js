b3153cbb0d906cc67fb4b2893c8413456fb93123
mayar-kabaja <kabajamayar@gmail.com>
2022-03-14 22:43:46 +0200

    create home page and styling it Relates #1

--- server/routes/routerPages.js
+++ server/routes/routerPages.js
@@ -3,7 +3,7 @@ const {
   homePage, loginPage, signupPage, profilePage,
 } = require('../controllers/index');
 
-routerPages.get('/home', homePage);
+routerPages.get(['/','/home'], homePage);
 routerPages.get('/login', loginPage);
 routerPages.get('/signup', signupPage);
 routerPages.get('/profile', profilePage);
